f8dfbd15e2b9503a58f009230a3aa20fcf942ee8
EC2 Default User <ec2-user@ip-172-31-88-130.ec2.internal>
2021-01-09 18:21:40 +0000

    Transate App II

--- todos/translate.py
+++ todos/translate.py
@@ -6,19 +6,40 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 
 
-class translation():
-    def __init__(self):
-        self.instance = boto3.client('translate', region_name='us-east-1', use_ssl=True)
-        
-    def get_item(self, id):
-        pass
-    
-    def translate_item(self, Text_, SourceLanguageCode_='auto', TargetLanguageCode_='en'):
-        return self.instance.translate_text(Text=Text_, SourceLanguageCode=SourceLanguageCode_, TargetLanguageCode=TargetLanguageCode_)
-        
-        
+#class translation():
+#    def __init__(self):
+#        self.instance = boto3.client('translate', region_name='us-east-1', use_ssl=True)
+#        
+#    def get_item(self, id):
+#        pass
+#    
+#    def translate_item(self, Text_, SourceLanguageCode_='auto', TargetLanguageCode_='en'):
+#        return self.instance.translate_text(Text=Text_, SourceLanguageCode=SourceLanguageCode_, TargetLanguageCode=TargetLanguageCode_)
+#        
+#        
+#
+#def translate():
+#    action = translate()
+#    translation = action.translate_item('Hola, soy Adrian')
+#    print(translation)
 
-def translate():
-    action = translate()
-    translation = action.translate_item('Hola, soy Adrian')
+
+def translate(event, context):
+    print('This is the event')
+    print(event)
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch todo from the database
+    result = table.get_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+    Text_ = 'I am trying'
+    SourceLanguageCode_ = 'auto'
+    TargetLanguageCode_ = 'es'
+
+    trnslt = boto3.client('translate', region_name='us-east-1', use_ssl=True)
+    translation = trnslt.translate_text(Text=Text_, SourceLanguageCode=SourceLanguageCode_, TargetLanguageCode=TargetLanguageCode_)
+    print('This is a translation:')
     print(translation)
