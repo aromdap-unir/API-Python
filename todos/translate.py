38febb03bd4bd5e5d37d0d1c4039984229e5f95e
Adrian Romero-Dapena <adrianromerodapena@riseup.net>
2021-01-10 12:18:18 +0100

    Translate Fix II

--- todos/translate.py
+++ todos/translate.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import json
 
@@ -16,22 +17,35 @@ def translate(event, context):
         }
     )
 
-    Text_ = result['Item']
-    SourceLanguageCode_ = event['pathParameters']['from']
-    TargetLanguageCode_ = event['pathParameters']['to']
-    
-    trnslt = boto3.client('translate', 
-                          region_name='us-east-1', 
-                          use_ssl=True)
-    translation = trnslt.translate_text(Text=Text_, 
-                                        SourceLanguageCode=SourceLanguageCode_, 
-                                        TargetLanguageCode=TargetLanguageCode_)
-    
-    # create a response
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(result['Item'],
-##        "body": json.dumps(result.get(‘TranslatedText’),
-                           cls=decimalencoder.DecimalEncoder)
-    }
+    try:
+        item = result['Item']
 
+        # comprehend origin language
+        comprehend = boto3.client(service_name='comprehend',
+                                  region_name='us-east-1')
+        related = comprehend.detect_dominant_language(Text=item['text'])
+
+        # translate result text
+        translate = boto3.client(service_name='translate',
+                                 region_name='us-east-1',
+                                 use_ssl=True)
+
+        translation = translate.translate_text(
+            Text=item['text'],
+            SourceLanguageCode=related['Languages'][0]['LanguageCode'],
+            TargetLanguageCode=event['pathParameters']['lang'])
+
+        item['text'] = translation['TranslatedText']
+
+        # create a response
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(item,
+                               cls=decimalencoder.DecimalEncoder)
+        }
+
+        return response
+
+    except Exception as e:
+        logging.error(str(e))
+        raise Exception("[ErrorMessage]: " + str(e))
