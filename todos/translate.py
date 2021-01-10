c9c708da774aa4106f2857423c95fd5c26694129
EC2 Default User <ec2-user@ip-172-31-88-130.ec2.internal>
2021-01-10 09:51:47 +0000

    Translate App III

--- todos/translate.py
+++ todos/translate.py
@@ -6,6 +6,34 @@ import boto3
 dynamodb = boto3.resource('dynamodb')
 
 
+def translate(event, context):
+    print('This is the event')
+    print(event)
+    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
+
+    # fetch todo from the database
+    result = table.get_item(
+        Key={
+            'id': event['pathParameters']['id']
+        }
+    )
+
+
+    Text_ = result['Item']
+    SourceLanguageCode_ = event['pathParameters']['from']
+    TargetLanguageCode_ = event['pathParameters']['to']
+    
+    trnslt = boto3.client('translate', region_name='us-east-1', use_ssl=True)
+    translation = trnslt.translate_text(Text=Text_, SourceLanguageCode=SourceLanguageCode_, TargetLanguageCode=TargetLanguageCode_)
+    print('This is a translation:')
+    print(translation)
+    
+        # create a response
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(result.get(‘TranslatedText’),
+                           cls=decimalencoder.DecimalEncoder)
+    }
 #class translation():
 #    def __init__(self):
 #        self.instance = boto3.client('translate', region_name='us-east-1', use_ssl=True)
@@ -22,24 +50,3 @@ dynamodb = boto3.resource('dynamodb')
 #    action = translate()
 #    translation = action.translate_item('Hola, soy Adrian')
 #    print(translation)
-
-
-def translate(event, context):
-    print('This is the event')
-    print(event)
-    table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
-
-    # fetch todo from the database
-    result = table.get_item(
-        Key={
-            'id': event['pathParameters']['id']
-        }
-    )
-    Text_ = 'I am trying'
-    SourceLanguageCode_ = 'auto'
-    TargetLanguageCode_ = 'es'
-
-    trnslt = boto3.client('translate', region_name='us-east-1', use_ssl=True)
-    translation = trnslt.translate_text(Text=Text_, SourceLanguageCode=SourceLanguageCode_, TargetLanguageCode=TargetLanguageCode_)
-    print('This is a translation:')
-    print(translation)
